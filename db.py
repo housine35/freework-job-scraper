159a045d29c4dd98ef50bdd32ebb61fcf39eb8ff
housine35 <housine35@hotmail.com>
2025-07-08 17:13:55 +0200

    adding freework scraper

--- db.py
+++ db.py
@@ -1,16 +1,20 @@
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote
 
 load_dotenv()
 
 def init_db():
     """Initialize MongoDB connection using environment variables."""
     user = os.getenv("MONGO_USER")
-    password = os.getenv("MONGO_PASSWORD")
+    password = quote(os.getenv("MONGO_PASSWORD"))  # URL-encode password
     host = os.getenv("MONGO_HOST")
     db_name = os.getenv("MONGO_DB")
     collection_name = os.getenv("MONGO_COLLECTION")
+
+    if not all([user, password, host, db_name, collection_name]):
+        raise ValueError("One or more environment variables are missing")
 
     uri = f"mongodb+srv://{user}:{password}@{host}/{db_name}?retryWrites=true&w=majority"
     client = MongoClient(uri)
